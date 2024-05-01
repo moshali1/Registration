a4950e8cc8d6ee4aeb870a8be1e25d6001e1073f
Mohamed Shiek <mshiek114@gmail.com>
2024-05-01 04:00:56 -0500

    Added Message Update for Prelim Competition

--- RegistrationPortal.Data/Models/BaseModels/EventScheduleInfo.cs
+++ RegistrationPortal.Data/Models/BaseModels/EventScheduleInfo.cs
@@ -3,9 +3,11 @@ public class EventScheduleInfo
 {
     public string PrelimScheduledDate { get; set; }
     public string PrelimScheduledTime { get; set; }
+    public bool CheckedPrelimSchedule { get; set; } = false; 
     public bool IsAttended { get; set; } = false;
     public bool IsQualified { get; set; } = false;
     public string FinalScheduledDate { get; set; }
     public string FinalScheduledTime { get; set; }
+    public bool CheckedFinalSchedule { get; set; } = false;
     public DateTime CheckInDateTime { get; set; }
 }
